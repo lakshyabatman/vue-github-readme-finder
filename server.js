99d341781b8686af22fb5a81e141533712f93a1a
Lakshya Khera <lakshya.khera@gmail.com>
2020-08-13 14:07:44 +0530

    Bug fixes

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
-const port = process.env.PORT || 8080
+const serveStatic = require('serve-static')
 const path = require('path')
+
 const app = express()
 
-app.use(express.static(path.join(__dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
 app.get(/.*/, function (req, res) {
-  res.sendFile(path.join(__dirname, '/dist/index.html'))
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
-app.listen(port, () => {
-  console.log('Server working')
-})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
